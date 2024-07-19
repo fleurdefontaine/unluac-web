ec6addd163a62362218320e7034aa376adb78bb0
fleurdefontaine <yttaanimek@gmail.com>
2024-07-19 14:52:43 +0800

    setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 echo "Installing dependencies"
+yes | pkg update
+yes | pkg upgrade
+pkg install git -y
+pkg install nodejs -y
+pkg install jdk-17 -y
 
 echo "Cloning repository"
 git clone https://github.com/fleurdefontaine/unluac-web.git zxc
